7aea18d61983325d0e2dd47deefbcb476eccb7a4
scan-dev <scan@vmlab.be>
2025-06-10 17:02:57 +0200

    vmlab(changes): rename bmaid

--- build-web-agent.cfg.js
+++ build-web-agent.cfg.js
@@ -2,8 +2,8 @@
 // This file contains the configuration for the build-web-agent.js script.
 
 module.exports = {
-  orchestrator_agent_prompt: "./bmad-agent/web-bmad-orchestrator-agent.md",
-  agent_cfg: "./bmad-agent/web-bmad-orchestrator-agent.cfg.md",
-  asset_root: "./bmad-agent/",
+  orchestrator_agent_prompt: "./bmaid-agent/web-bmaid-orchestrator-agent.md",
+  agent_cfg: "./bmaid-agent/web-bmaid-orchestrator-agent.cfg.md",
+  asset_root: "./bmaid-agent/",
   build_dir: "./build/",
 };
